5d7dd66bd159df51681ecad43b0df8b8f373d289
ayal <ayalg@wix.com>
2022-09-10 20:04:53 +0300

    sync-unsync works with timestamp of auto scroll

--- src/App.js
+++ src/App.js
@@ -35,29 +35,31 @@ class ScrollPromise {
   }
 }
 
-const mobileCheck = function() {
+const mobileCheck = function () {
   let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 };
 
 
 
-const Script = ({ textMap, syncScroll, setSyncScroll, shouldSyncScroll, current, videoRef, setCurrentTime, setDirection }) => {
+const Script = ({ scrollState, textMap, syncScroll, setSyncScroll, shouldSyncScroll, current, videoRef, setCurrentTime, setDirection }) => {
   const debouncedScroll = useMemo(() => debounce(() => {
-    console.log('finished scrolling debounce', Date.now());
+    console.log('finished scrolling debounce', Date.now(), 'auto?', scrollState.current.auto);
   }, 1000), []);
+
   const scrollHandler = useCallback(() => {
     debouncedScroll()
   }, [debouncedScroll])
 
-  return <div style={{ display:'flex', flexDirection: 'column', overflow: 'hidden' }}>
+  return <div style={{ display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
     <div style={{ display: 'flex', height: '30px' }}>
       <button onClick={() => {
         console.log('asked to sync scroll');
         syncScroll();
         setSyncScroll(true);
       }}>Sync</button>
+      <div>{shouldSyncScroll ? 'synced':'unsynced'}</div>
       <button onClick={() => {
         setDirection('vertical');
       }}>Vertical</button>
@@ -65,9 +67,13 @@ const Script = ({ textMap, syncScroll, setSyncScroll, shouldSyncScroll, current,
         setDirection('horizontal');
       }}>Horizontal</button>
     </div>
-    <div style={{ display: 'flex', flex: '1', flexDirection: 'column', overflow: 'auto' }} onScroll={() => {
-      //setSyncScroll(false);
-      console.log('scrolling...');
+    <div id="text-container" style={{ display: 'flex', flex: '1', flexDirection: 'column', overflow: 'auto' }} onScroll={() => {
+      const isAutoScrolling = Date.now() - scrollState.current.auto < 1000;
+      if (!isAutoScrolling) {
+        console.log('>>> MANUAL SCROLL - UNSYNCHING');
+        setSyncScroll(false);
+      }
+      console.log('scrolling...', 'auto?', scrollState.current.auto);
       scrollHandler()
     }}>
       {
@@ -85,6 +91,20 @@ const Script = ({ textMap, syncScroll, setSyncScroll, shouldSyncScroll, current,
         })
       }
     </div>
+  </div>
+}
+
+const Video = ({ videoRef, currentTime, setSyncScroll, setCurrentTime, videoUrl }) => {
+  return <div style={{ display: 'flex', background: 'black', color: 'white', overflow: 'hidden' }}>
+    <video ref={videoRef} onTimeUpdate={(e) => {
+      const delta = Math.abs(e.target.currentTime - currentTime);
+      if (delta > 3) { // manual seek in video
+        console.log('delta big - setting sync manually');
+        setSyncScroll(true);
+      }
+      console.log('setting current time');
+      setCurrentTime(e.target.currentTime)
+    }} controls='on' playsInline={true} style={{ width: '100%' }} src={videoUrl} />
   </div>
 }
 
@@ -128,10 +148,13 @@ function Comp({ videoUrl, textMap }) {
   }, [currentTime]);
 
 
+  const scrollState = useRef({})
+
   const syncScroll = useCallback(() => {
     const doScroll = async () => {
       if (current) {
         const element = document.querySelector(`#text-part-${current.id}`);
+        scrollState.current.auto = Date.now();
         console.log('actually scrolling to view');
         element?.scrollIntoView({ behavior: 'smooth', block: 'center' });
       }
@@ -140,6 +163,7 @@ function Comp({ videoUrl, textMap }) {
   }, [current])
 
   useEffect(() => {
+    console.log('scroll effect');
     if (!shouldSyncScroll) {
       console.log('not syncing');
       return;
@@ -147,7 +171,7 @@ function Comp({ videoUrl, textMap }) {
     syncScroll()
   }, [shouldSyncScroll, syncScroll])
 
-
+  const videoProps = { videoRef, currentTime, setSyncScroll, setCurrentTime, videoUrl };
 
   return (
     <div className="App" style={{ display: 'flex', width: '100%', height: '100vh', flexDirection: 'row', overflow: 'hidden' }}>
@@ -155,21 +179,14 @@ function Comp({ videoUrl, textMap }) {
       <Split direction={direction} render={({ getGridProps, getGutterProps }) => {
         const props = getGridProps();
         const gutterProps = getGutterProps(horizontal ? 'column' : 'row', 1);
-        const initial = horizontal ?  { gridTemplateColumns: '2fr 16px 1fr' } : { gridTemplateRows: '1fr 16px 2fr' }
-        const overrides = !horizontal ?  { gridTemplateColumns: '' } : { gridTemplateRows: '' }
+        const initial = horizontal ? { gridTemplateColumns: '2fr 16px 1fr' } : { gridTemplateRows: '1fr 16px 2fr' }
+        const overrides = !horizontal ? { gridTemplateColumns: '' } : { gridTemplateRows: '' }
         return (
-          <div {...props} style={{ display: 'grid',...initial, ...props.style, ...overrides }}>
-            <div style={{ display:'flex', background: 'black', color: 'white', overflow: 'hidden' }}>
-              <video ref={videoRef} onTimeUpdate={(e) => {
-                const delta = Math.abs(e.target.currentTime - currentTime);
-                if (delta > 3) { // manual seek in video
-                  setSyncScroll(true);
-                }
-                setCurrentTime(e.target.currentTime)
-              }} controls='on' playsInline={true} style={{ width: '100%' }} onLoad={() => setVideoLoaded(true)} src={videoUrl} />
-            </div>
-            <div {...gutterProps} className="gutter" style={{...gutterProps.style, cursor: horizontal ? 'col-resize' : 'row-resize'}} />
+          <div {...props} style={{ display: 'grid', ...initial, ...props.style, ...overrides }}>
+            <Video {...videoProps} />
+            <div {...gutterProps} className="gutter" style={{ ...gutterProps.style, cursor: horizontal ? 'col-resize' : 'row-resize' }} />
             <Script
+              scrollState={scrollState}
               direction={direction}
               setDirection={setDirection}
               textMap={textMap}
