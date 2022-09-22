f3809ed9a1a9cfdd4cb4b1706f2d20515d41d31f
ayal <ayalg@wix.com>
2022-09-22 17:18:37 +0200

    only text is selected

--- src/Script.js
+++ src/Script.js
@@ -35,13 +35,13 @@ export const Script = ({ percentage, scrollState, textMap, textError, syncScroll
               setScrollSynced(true);
               setCurrentTime(tm.start);
             }} key={`text-part-${tm.id}`} className={`text-part ${!tm.noName ? 'has-name' : 'no-name'}`} id={`text-part-${tm.id}`}
-              style={{ background: isCurrent ? selectedColor : 'none', flexDirection: 'column', position: 'relative' }}>
+              style={{ flexDirection: 'column', position: 'relative' }}>
               <div style={{ height: '2px', width: '100%', position: 'absolute', top: '0', left: '0' }}>
                 {isCurrent ? <div style={{ height: '2px', width: isCurrent ? `${percentage * 100}%` : '100%', transition: 'width 0.5s', background: 'red' }}></div> : null}
               </div>
               <div className="text-start-time" style={{ fontSize: '12px' }}>{`${tm.startTs.split('.')[0]}`}</div>
               <div className="text-part-name" style={{ color: tm.color, fontWeight: 'bold' }}>{tm.name}</div>
-              <div>{tm.text}</div>
+              <div className="text-part-text" style={{background: isCurrent ? selectedColor : 'none'}}>{tm.text}</div>
             </div>
           }) :
           <div style={{ margin: 'auto', display: 'flex', textAlign: 'center', justifyContent: 'center' }}>
